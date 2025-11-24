510e28c1f0f1ada6338a501be2dacc3c852b6bef
Inamullah <sheenzada7@gmail.com>
2025-11-24 04:51:40 -0800

    response

---
+++ .vscode/mul.js
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Stars</title>
+    </head>
+    <body>
+        <script>
+             for (int i=1 ; i<5; i++){
+            for(int j=5 ; j> 0; j--){
+                document.writeln("");
+            }
+            
+            document.write("<br>")
+        }
+        </script>
+    </body>
+</html>
