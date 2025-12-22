b7d2dbfb6fb54c8d5fc1c4c59f5549b0e52019a0
Inamullah <sheenzada7@gmail.com>
2025-12-22 02:47:17 -0800

    function

---
+++ javascript/arrow.function.js
@@ -0,0 +1,19 @@
+let fun=() => {
+    console.log("hello");
+    
+}
+fun()
+
+
+let single = () => console.log("The function is running");
+single()
+
+
+let sum=(a,b) => a+b ;
+console.log(sum(3,3));
+
+let sums=(a,b) => {
+    return a+b
+} ;
+console.log(sums(4,2));
+
