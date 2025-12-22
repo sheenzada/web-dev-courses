7e9618265cafabed0f31aa251d343e7d6bfcb287
Inamullah <sheenzada7@gmail.com>
2025-12-22 02:35:32 -0800

    function

---
+++ javascript/function.js
@@ -0,0 +1,24 @@
+// function sum(){
+//     console.log("function running");
+    
+// }
+// sum()
+
+// function sum(a,s){
+//     return a+s;
+// }
+// let sm=sum(2,2)
+// console.log(sm);
+
+// function sum(a,s,d) {
+//     return a+s*d;
+// }
+// let sums=sum(3,4,2)
+// console.log(sums);
+
+function smd(a,...s){
+    return a*s;
+}
+console.log(smd(2,3,56,7,8));
+
+
