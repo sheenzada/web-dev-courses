e366a34cabeb89b1390b4a833790f0270adf3277
Inamullah <sheenzada7@gmail.com>
2025-12-22 02:57:26 -0800

    function

---
+++ javascript/paractice.arrow.function.js
@@ -0,0 +1,19 @@
+let fun = () => {
+    console.log("The arrow function is running");
+    
+}
+fun()
+
+let single = () => console.log("The singel arrow function is running");
+fun(single)
+
+let sum=(a,b) => a+b ;
+    console.log(sum(2,3));
+
+    
+let add = (c,d) => {
+    return c+d;
+}
+console.log(add(6,6));
+
+
