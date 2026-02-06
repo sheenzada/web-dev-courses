061855b34a26f51df9cf22eea34ee11e11e23dcb
Inamullah <sheenzada7@gmail.com>
2026-02-06 03:54:14 -0800

    js

---
+++ javascript/what.js
@@ -0,0 +1,64 @@
+// console.log("this is a first program");
+// console.log("runing..........");
+// console.log("nodmon is working");
+
+// console.log("class end");
+
+
+var a=43;
+var user="admin"
+
+var user="user"
+console.log(user);
+
+var a=12;
+console.log(a);
+const b=32;
+// const b=54;
+console.log(b);
+let users="admin"
+console.log(users);
+// let users="user";
+console.log(users);
+
+console.log(true+1);
+console.log(false*10);
+
+// operator
+// = asssign ope
+// <> 
+// ! not equal
+// comp
+// == eq
+// === eq to 
+// !=
+// <= >=
+
+// logcal op
+
+// and && oper
+// T T T
+// T F F
+// F T F
+// F F F
+
+// OR OPR ||
+// T T T 
+// T F T
+// F T T
+// F F F
+
+// NOT OP !
+// T F 
+// F T
+
+// Arthamatic
+// +
+// -
+// *
+// /
+// %
+
+
+
+//
